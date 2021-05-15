4cb0a0d82a85baf611de5f6e09b3f61735d50c52
poppymills2@gmail.com <poppymills2@gmail.com>
2021-05-15 12:01:29 -0400

    promotion and partner routers

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -25,4 +25,31 @@ campsiteRouter
     res.end("Deleting all campsites");
   });
 
+campsiteRouter
+  .route("/:campsiteId")
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next();
+  })
+  .get((req, res) => {
+    res.end(
+      `Will send all the campsites with id ${req.params.campsiteId} to you`
+    );
+  })
+  .post((req, res) => {
+    res.end(
+      `Will add the campsite: ${req.body.name} with description: ${req.body.description}`
+    );
+  })
+  .put((req, res) => {
+    res.write(`Updating the campsites: ${req.params.campsiteId}\n`);
+    res.end(
+      `Will send the campsite: ${req.body.name} with description: ${req.body.description}`
+    );
+  })
+  .delete((req, res) => {
+    res.end(`Deleting all campsites with Id: ${req.params.campsiteId}`);
+  });
+
 module.exports = campsiteRouter;
